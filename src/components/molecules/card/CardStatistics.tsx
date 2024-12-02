855ba3530e277bfb20981c05d3c765429e7ea20a
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-02 15:56:45 +0700

    add: CardUserProfile feat/profile

--- src/components/molecules/card/CardStatistics.tsx
+++ src/components/molecules/card/CardStatistics.tsx
@@ -1,55 +1,45 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Flame, Trophy, Zap } from "lucide-react";
 import Link from "next/link";
 
 export default function CardStatistics() {
   return (
     <>
-      <Card className="bg-transparent">
-        <CardHeader className="font-bold text-2xl">Statistik</CardHeader>
-        <CardContent>
-          <div className="flex flex-col justify-center items-center">
-            <div className="flex flex-row gap-4 w-full items-center justify-center mb-4">
-              <div className="border rounded-lg flex flex-row items-center justify-start w-1/2 gap-4 p-4">
-                <Flame
-                  className="text-orange-500 w-10 h-10"
-                  fill="currentColor"
-                />
-                <div className="flex flex-col">
-                  <h1 className="font-bold text-xl">2</h1>
-                  <h1 className="text-sm font-light opacity-80">
-                    Runtunan hari
-                  </h1>
-                </div>
-              </div>
-              <div className="border rounded-lg flex flex-row items-center justify-start w-1/2 gap-4 p-4">
-                <Zap className="h-8 w-8 text-yellow-400" fill="currentColor" />
-                <div className="flex flex-col">
-                  <h1 className="font-bold text-xl">1260</h1>
-                  <h1 className="text-sm font-light opacity-80">Total XP</h1>
-                </div>
-              </div>
-            </div>
-            <div className="border rounded-lg flex flex-row items-center justify-between gap-4 w-full p-4">
-              <div className="flex flex-row gap-4">
-                <Trophy className="h-10 w-10 text-yellow-400" />
-                <div className="flex flex-col">
-                  <h1 className="font-bold text-xl">24th</h1>
-                  <h1 className="text-sm font-light opacity-80">
-                    Peringkat saat ini
-                  </h1>
-                </div>
-              </div>
-              <Link
-                href={"#"}
-                className="text-sm font-bold uppercase text-primary hover:underline"
-              >
-                Lihat Semua
-              </Link>
+      <h1 className="text-2xl font-bold">Statistik</h1>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-row gap-4 w-full items-center justify-center mb-4">
+          <div className="border rounded-lg flex flex-row items-center justify-start w-1/2 gap-4 p-4">
+            <Flame className="text-orange-500 w-12 h-12" fill="currentColor" />
+            <div className="flex flex-col">
+              <h1 className="font-bold text-xl">2</h1>
+              <h1 className="text-sm font-light opacity-80">Runtunan hari</h1>
             </div>
           </div>
-        </CardContent>
-      </Card>
+          <div className="border rounded-lg flex flex-row items-center justify-start w-1/2 gap-4 p-4">
+            <Zap className="h-10 w-10 text-yellow-400" fill="currentColor" />
+            <div className="flex flex-col">
+              <h1 className="font-bold text-xl">1260</h1>
+              <h1 className="text-sm font-light opacity-80">Total XP</h1>
+            </div>
+          </div>
+        </div>
+        <div className="border rounded-lg flex flex-row items-center justify-between gap-4 w-full p-4">
+          <div className="flex flex-row gap-4">
+            <Trophy className="h-12 w-12 text-yellow-400" />
+            <div className="flex flex-col">
+              <h1 className="font-bold text-xl">24th</h1>
+              <h1 className="text-sm font-light opacity-80">
+                Peringkat saat ini
+              </h1>
+            </div>
+          </div>
+          <Link
+            href={"#"}
+            className="text-sm font-bold uppercase text-primary hover:underline"
+          >
+            Lihat Semua
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/organism/dashboard/user/profile/ProfileDashboardWrapper.tsx
+++ src/components/organism/dashboard/user/profile/ProfileDashboardWrapper.tsx
@@ -1,12 +1,14 @@
 import CardDailyMission from "@/components/molecules/card/CardDailyMission";
 import CardProfile from "@/components/molecules/card/CardProfile";
 import CardStatistics from "@/components/molecules/card/CardStatistics";
+import CardUserProfile from "@/components/molecules/card/CardUserProfile";
 
 export default function ProfileDashboardWrapper() {
   return (
     <>
       <div className="flex flex-col md:flex-row md:gap-12 gap-8">
         <div className="md:w-8/12 w-full space-y-6">
+          <CardUserProfile />
           <CardStatistics />
         </div>
         <div className="md:w-4/12 w-full space-y-6">
