0263d867a12a9bb2e1ff72461027e8332b469b42
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-11 05:47:09 +0700

    fix: seperate matching fair to component

--- src/components/organism/home/Reason.tsx
+++ src/components/organism/home/Reason.tsx
@@ -1,48 +1,13 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { MatchingPair } from "@/components/molecules/card/CardMatchingPairs";
 
 import flowchart from "/public/images/flowchart.gif";
 import Image from "next/image";
 
 export default function Reason() {
-  const [selected, setSelected] = useState<{
-    id: string;
-    label: string;
-  } | null>(null);
-  const [matches, setMatches] = useState<{ [key: string]: string }>({
+  const matches = {
     String: 'var x = "Hello"',
     Integer: "var x = 12",
-  });
-  const [correctPairs, setCorrectPairs] = useState<string[]>([]);
-  const [temporarilyDisabled, setTemporarilyDisabled] = useState<string[]>([]);
-
-  const handleButtonClick = (id: string, label: string) => {
-    if (selected) {
-      if (matches[selected.label] === label) {
-        // Cocok
-        setCorrectPairs((prev) => [...prev, selected.label, label]);
-        setSelected(null); // Reset pilihan
-      } else {
-        // Tidak cocok
-        setTemporarilyDisabled((prev) => [...prev, selected.label, label]);
-        setTimeout(() => {
-          setTemporarilyDisabled((prev) =>
-            prev.filter((item) => item !== selected.label && item !== label)
-          );
-        }, 1000); // Jeda 1 detik
-        setSelected(null); // Reset pilihan
-      }
-    } else {
-      // Pilih tombol pertama
-      setSelected({ id, label });
-    }
   };
-
-  const isCorrect = (label: string) => correctPairs.includes(label);
-  const isTemporarilyDisabled = (label: string) =>
-    temporarilyDisabled.includes(label);
-  const isSelected = (label: string) => selected?.label === label;
 
   return (
     <>
@@ -50,49 +15,8 @@ export default function Reason() {
         <h1 className="text-green-500 font-bold text-2xl tracking-wider text-center">
           Kenapa Memilih CodeLingo?
         </h1>
-        <div className="flex flex-row items-center justify-center gap-20 my-24">
-          <div className="flex flex-col gap-4">
-            {["String", "Integer"].map((item, index) => (
-              <Button
-                key={index}
-                className={`w-36 rounded-lg transition-transform duration-500 ease-in-out transform ${
-                  isCorrect(item)
-                    ? "bg-green-500"
-                    : isTemporarilyDisabled(item)
-                    ? "bg-red-500 opacity-50 scale-95"
-                    : isSelected(item)
-                    ? "border-2 border-blue-500"
-                    : ""
-                }`}
-                variant={"secondary"}
-                onClick={() => handleButtonClick(`left-${index}`, item)}
-                disabled={isCorrect(item) || isTemporarilyDisabled(item)}
-              >
-                {item}
-              </Button>
-            ))}
-          </div>
-          <div className="flex flex-col gap-4">
-            {['var x = "Hello"', "var x = 12"].map((item, index) => (
-              <Button
-                key={index}
-                className={`w-36 rounded-lg transition-transform duration-500 ease-in-out transform ${
-                  isCorrect(item)
-                    ? "bg-green-500"
-                    : isTemporarilyDisabled(item)
-                    ? "bg-red-500 opacity-50 scale-95"
-                    : isSelected(item)
-                    ? "border-2 border-blue-500"
-                    : ""
-                }`}
-                variant={"secondary"}
-                onClick={() => handleButtonClick(`right-${index}`, item)}
-                disabled={isCorrect(item) || isTemporarilyDisabled(item)}
-              >
-                {item}
-              </Button>
-            ))}
-          </div>
+        <div className="my-24">
+          <MatchingPair pairs={matches} />
         </div>
         <div className="flex flex-col items-center space-y-4 pb-8">
           <h1 className="text-[#38B5FC] text-2xl font-bold">
@@ -106,7 +30,7 @@ export default function Reason() {
         </div>
         <div className="flex flex-col items-center space-y-4">
           <Image src={flowchart} alt="flowchart" width={500} height={500} />
-          <h1 className="text-[#38B5FC] text-2xl font-bold ">
+          <h1 className="text-[#38B5FC] text-2xl font-bold">
             Pendekatan Ramah Anak
           </h1>
           <p className="w-1/2 text-center opacity-80">
