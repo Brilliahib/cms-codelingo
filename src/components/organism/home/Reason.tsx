f3786d160c4c43afbe05855883f75225120f8869
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-11 08:45:31 +0700

    fix: responsif

--- src/components/organism/home/Reason.tsx
+++ src/components/organism/home/Reason.tsx
@@ -12,7 +12,7 @@ export default function Reason() {
 
   return (
     <>
-      <div className="my-24 flex flex-col items-center">
+      <div className="my-24 flex flex-col items-center px-8">
         <h1 className="text-green-500 font-bold text-2xl tracking-wider text-center">
           Kenapa Memilih CodeLingo?
         </h1>
@@ -22,10 +22,10 @@ export default function Reason() {
           <Image src={pairs} alt="pairs" width={500} height={500} />
         </div>
         <div className="flex flex-col items-center space-y-4 pb-8">
-          <h1 className="text-[#38B5FC] text-2xl font-bold">
+          <h1 className="text-[#38B5FC] md:text-2xl text-xl font-bold">
             Program Belajar Interaktif
           </h1>
-          <p className="w-1/2 text-center opacity-80">
+          <p className="md:w-1/2 text-center opacity-80">
             Anak-anak belajar coding dengan materi visual yang menarik dan kuis
             interaktif seperti permainan, sehingga belajar terasa seru dan
             menyenangkan.
@@ -33,10 +33,10 @@ export default function Reason() {
         </div>
         <div className="flex flex-col items-center space-y-4 pb-8">
           <Image src={flowchart} alt="flowchart" width={500} height={500} />
-          <h1 className="text-[#38B5FC] text-2xl font-bold">
+          <h1 className="text-[#38B5FC] md:text-2xl text-xl font-bold">
             Pendekatan Ramah Anak
           </h1>
-          <p className="w-1/2 text-center opacity-80">
+          <p className="md:w-1/2 text-center opacity-80">
             Pembelajaran dirancang khusus agar sesuai dengan kebutuhan
             anak-anak, menggunakan gambar berwarna-warni, animasi interaktif,
             dan narasi sederhana yang mudah dipahami.
@@ -45,10 +45,10 @@ export default function Reason() {
         <div className="flex flex-col items-center space-y-4 pb-8">
           {/* animasi tambahan lainnya? */}
           <Image src={flowchart} alt="flowchart" width={500} height={500} />
-          <h1 className="text-[#38B5FC] text-2xl font-bold">
+          <h1 className="text-[#38B5FC] md:text-2xl text-xl font-bold">
             Belajar Seperti Bermain
           </h1>
-          <p className="w-1/2 text-center opacity-80">
+          <p className="md:w-1/2 text-center opacity-80">
             Pembelajaran dikemas seperti permainan, di mana anak-anak bisa
             mengumpulkan EXP, mencapai level baru, dan bersaing di liga
             leaderboard yang akan meningkatkan rasa percaya diri dan semangat
