ffb22eae147ab71316a552626cbdd3f9d1501a97
Muhammad Ahib Ibrilli <119148205+Brilliahib@users.noreply.github.com>
2024-12-14 21:47:35 +0700

    chore: update conflict

--- src/components/organism/home/ContentPanda.tsx
+++ src/components/organism/home/ContentPanda.tsx
@@ -5,11 +5,8 @@ import { Zap } from "lucide-react";
 export default function ContentPanda() {
   return (
     <>
-<<<<<<< HEAD
       <div className="bg-[#273856] w-full flex flex-col md:flex-row items-center justify-center gap-16 py-12  ">
-=======
       <div className="bg-[#273856] w-full flex flex-col md:flex-row items-center justify-center gap-12 pad-x md:py-16 py-12">
->>>>>>> 768737113bd4868b4b6e6cb8a035df0724546e2f
         <Image
           src={learnPanda}
           alt="learn panda"
@@ -17,11 +14,8 @@ export default function ContentPanda() {
           height="1000"
           className="md:max-w-[300px] max-w-[200px]"
         />
-<<<<<<< HEAD
         <div className="md:w-6/12 text-justify space-y-6">
-=======
         <div className="md:w-6/12 text-justify space-y-4">
->>>>>>> 768737113bd4868b4b6e6cb8a035df0724546e2f
           <div className="flex items-center gap-4">
             <Image src={"/images/zap.svg"} alt="Petir" width={32} height={32}/>
             <h1 className="font-bold tracking-wider text-primary md:text-xl text-2xl">
