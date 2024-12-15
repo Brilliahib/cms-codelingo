015996672d7a8978ce2d3b268ffaddb9748fc0f2
brilliahib <brilliahib21@gmail.com>
2024-12-15 12:15:40 +0700

    feat: create new question for admin

---
+++ src/validators/admin/question/question-validator.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const questionSchema = z.object({
+  quiz_id: z.string().min(1, { message: "Quiz ID wajib diisi" }),
+  question_text: z
+    .string()
+    .min(1, { message: "Pertanyaan wajib diisi" })
+    .max(255, { message: "Pertanyaan maksimal 255 karakter" }),
+  question_image: z
+    .union([
+      z.string().nullable().optional(),
+      z
+        .instanceof(File)
+        .refine(
+          (file) =>
+            ["image/jpeg", "image/png", "image/jpg", "image/gif"].includes(
+              file.type
+            ),
+          { message: "Gambar harus berformat jpeg, png, jpg, atau gif" }
+        ),
+    ])
+    .optional(),
+  answers: z.array(
+    z.object({
+      answer_text: z.string().min(1, { message: "Jawaban tidak boleh kosong" }),
+      is_correct: z.boolean().transform((val) => (val ? 1 : 0)),
+    })
+  ),
+});
+
+export type QuestionType = z.infer<typeof questionSchema>;
